d2deaed2933449fe424623072c03c3e0dd9e0560
Maxim Sukharev <maxim@sourced.tech>
2018-06-27 19:07:02 +0200

    scroll uast viewer to highlighted node
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Item({ children, hovered, highlighted, ...rest }) {
-  let className = 'uast-item';
+function Item({ children, hovered, highlighted, className, ...rest }) {
+  let newClassName = `${className} uast-item`;
   if (hovered) {
-    className += ' hovered';
+    newClassName += ' hovered';
   }
   if (highlighted) {
-    className += ' highlighted';
+    newClassName += ' highlighted';
   }
 
   return (
-    <div className={className} {...rest}>
+    <div className={newClassName} {...rest}>
       {children}
     </div>
   );
 }
 
 Item.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
   hovered: PropTypes.bool,
   highlighted: PropTypes.bool
